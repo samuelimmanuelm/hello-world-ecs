782ef56a6e000b854dbc73004c7c63700ce8e102
Samuel Immanuel <125296834+samuelimmanuelm@users.noreply.github.com>
2024-06-07 23:33:12 +0530

    added-terraform ecs

---
+++ terraform-ecs/main.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_ecs_cluster" "hello_world_cluster" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "hello_world_task" {
+  family                   = "hello-world-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name      = "hello-world-app"
+    image     = "samuelimmanuel/hello-world-app:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "hello_world_service" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.hello_world_cluster.id
+  task_definition = aws_ecs_task_definition.hello_world_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.public.id]
+    security_groups = [aws_security_group.ecs_service_sg.id]
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "ecs_service_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
